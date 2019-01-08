8b3a729678aa90f5cc9b2bab973091c1a91598cd
Parker Couch <parkercouch@gmail.com>
2019-01-08 08:16:47 -0800

    Added comments, no styling

--- controllers/posts.js
+++ controllers/posts.js
@@ -32,11 +32,28 @@ router.get('/new', function(req, res) {
 router.get('/:id', function(req, res) {
   db.post.find({
     where: { id: req.params.id },
-    include: [db.author]
+    include: [db.author, db.comment]
   })
   .then(function(post) {
     if (!post) throw Error();
+    // console.log(post.comments)
     res.render('posts/show', { post: post });
+  })
+  .catch(function(error) {
+    res.status(400).render('main/404');
+  });
+});
+
+
+// POST /posts/:id/comments
+router.post('/:id/comments', function(req, res) {
+  db.comment.create({
+    name: req.body.name,
+    content: req.body.content,
+    postId: req.params.id,
+  })
+  .then(function() {
+    res.redirect(`/posts/${req.params.id}`);
   })
   .catch(function(error) {
     res.status(400).render('main/404');
